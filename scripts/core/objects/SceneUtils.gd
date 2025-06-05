b293b263437cfee722257241247261efb326bb6d
Artem Namednev <artem.namednev@gmail.com>
2025-06-05 18:57:27 +0400

    SceneUtils upgrade

--- scripts/core/objects/SceneUtils.gd
+++ scripts/core/objects/SceneUtils.gd
@@ -18,23 +18,68 @@ static func find_singleton_child_in(node: Node, type: StringName, recursive: boo
 	return candidates[0]
 
 
-## Attaches the given [param node] as a child of [param parent] and sets its owner to [param parent].
-## This is useful when dynamically adding a node that should be part of the scene tree and saved with it.
-static func attach_node_to(node: Node, parent: Node):
+## Reattaches the given [param node] to a new [param parent].
+## If the node already has a different parent, it will be detached first.
+static func reattach_node_to(node: Node, parent: Node) -> void:
+	if not node:
+		push_error("Node to attach is null.")
+		return
+	if not parent:
+		push_error("Parent node to attach to is null.")
+		return
+
+	var existed_parent := node.get_parent()
+	if existed_parent == parent:
+		push_warning("Node is already a child of the specified parent.")
+		return
+
+	if existed_parent:
+		existed_parent.remove_child(node)
+
 	parent.add_child(node)
 	node.set_owner(parent)
 
 
-## Detaches the given [param node] from its [param parent] and clears its owner.
+## Attaches the given [param node] as a child of [param parent] and sets its owner to [param parent].
+## This is useful when dynamically adding a node that should be part of the scene tree and saved with it.
+static func attach_node_to(node: Node, parent: Node) -> void:
+	if not node:
+		push_error("Node to attach is null.")
+		return
+	if not parent:
+		push_error("Parent node to attach to is null.")
+		return
+
+	var existed_parent := node.get_parent()
+	if existed_parent:
+		if existed_parent == parent:
+			push_warning("Node is already a child of the specified parent.")
+		else:
+			push_error("Node is already a child of an another node.")
+		return
+
+	parent.add_child(node)
+	node.set_owner(parent)
+
+
+## Detaches the given [param node] from its parent and clears its owner.
 ## This can be used to safely remove a node from the scene tree without freeing it yet.
-static func detach_node_from(node: Node, parent: Node):
-	if node.get_parent() == parent:
-		node.set_owner(null)
-		parent.remove_child(node)
+static func detach_node(node: Node) -> void:
+	if not node:
+		push_error("Node to detach is null.")
+		return
+
+	var parent := node.get_parent()
+	if not parent:
+		push_warning("Node is already detached.")
+		return
+
+	node.set_owner(null)
+	parent.remove_child(node)
 
 
-## Frees the given [param node] after detaching it from its [param parent].
-## Combines [method detach_node_from] and [method queue_free] to safely remove and delete a node.
-static func free_node_from(node: Node, parent: Node):
-	detach_node_from(node, parent)
-	node.queue_free()
+## Frees the given [param node] after detaching it from its parent.
+## Combines [method detach_node] and [method Node.queue_free] to safely remove and delete a node.
+static func free_node(node: Node):
+	detach_node(node)
+	if node: node.queue_free()
